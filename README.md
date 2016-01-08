3d6a2ad5b04a0badbad84225eef411cf8ffea827
Chris Montgomery <chr.montgomery@gmail.com>
2016-01-08 13:48:31 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Shrink Font to Fit jQuery Plugin
+
+> Shrinks the target text font so that the text will never wrap
+
+## Demo
+
+https://jsbin.com/bucata/3/edit?html,css,js,output
+
+## Examples
+
+### Simple
+
+```js
+$('.my-line').shrinkFontToFit();
+```
+
+### Options
+
+```js
+$('.my-line').shrinkFontToFit({
+  minFontSize: 8,
+  wrapAtMinSize: true
+});
+```
---
+++ jquery.shrinkfonttofit.js
@@ -0,0 +1,37 @@
+/*global jQuery */
+(function ($) {
+
+  $.fn.shrinkFontToFit = function (opts) {
+
+    var defaultOpts = $.extend({
+      minFontSize: 6,
+      wrapAtMinSize: false
+    }, opts);
+
+    function fontReducer($target) {
+      if ($target.prop('scrollWidth') > $target.width()) {
+        var newFontSize = parseInt($target.css('font-size')) - 1;
+        if (newFontSize < defaultOpts.minFontSize) { // don't allow smaller than this
+          // hide overflow text
+          if (defaultOpts.wrapAtMinSize) {
+            $target.css('white-space', 'normal');
+            return;
+          }
+          $target.css('overflow', 'hidden')
+            .css('text-overflow', 'ellipsis');
+          return;
+        }
+        $target.css('font-size', newFontSize + 'px');
+        return fontReducer($target);
+      }
+      return;
+    }
+
+    return this.each(function () {
+      $(this).css('white-space', 'nowrap');
+      fontReducer($(this));
+    });
+
+  };
+
+})(jQuery);
